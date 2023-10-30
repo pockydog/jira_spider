3fc9b0b071f4b1f4a727859e711d3d1bb8ed4dc4
user <user@userdeMacBook-Pro.local>
2023-10-30 13:32:28 +0800

    remove s

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from core.vicky_report import JiraByPeople
-from core.weekly_report import JiraByAll
-from core.pocky_dog import JiraByProject
+
 from core.Jirs_spider import JiraTest
 
 if __name__ == '__main__':
-    # JiraByAll.export_excel_test(this_week=False)
-    # JiraByProject.get_person_info(this_week=False)
-    # JiraByPeople.get_person_info(this_week=False)
     JiraTest.get_person_info(this_week=False)
